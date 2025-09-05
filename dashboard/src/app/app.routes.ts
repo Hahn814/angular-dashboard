ee16e796fe70451f7624823308e559d9eb86ce74
Paul Hahn <paulhahn18@gmail.com>
2025-09-05 12:19:58 +0000

    docs(routes): add TODO for smart task route implementation

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ShellComponent } from './layout/shell/shell.component';
 
+// TODO: Add smart task route
 export const routes: Routes = [
 	{
 		path: '',
